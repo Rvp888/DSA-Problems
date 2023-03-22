73b4770f5d3b5b37bc7cc2e8a79b4a89b1c26dea
Rohan Palankar <rohanpalankar8@gmail.com>
2023-03-22 10:30:53 +0530

    modify file

--- highestFrequency.js
+++ highestFrequency.js
@@ -14,3 +14,14 @@
 // Ex:
 // Input: arr = [1, 2, 3, 4, 5, 4, 3, 2, 1, 3, 4]
 // Output: 3.
+
+// Explaination:
+// The different types of birds occur in the following frequencies:
+
+// Type 1: 2
+// Type 2: 2
+// Type 3: 3
+// Type 4: 3
+// Type 5: 1
+
+// Both Type 3 and Type 4 have a frequency of 3, and the lower of those is Type 3.

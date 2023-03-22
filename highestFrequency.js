9df7913a093e1a96fc00f62397b264b4b8940ce2
Rohan Palankar <rohanpalankar8@gmail.com>
2023-03-22 10:28:24 +0530

    modify file

--- highestFrequency.js
+++ highestFrequency.js
@@ -4,3 +4,13 @@
 // Problem:
 // Given an array of bird sightings where every element represents a bird type id, determine the id of the most frequently 
 // sighted type. If more than 1 type has been spotted that maximum amount, return the smallest of their ids.
+
+// Example:
+// arr = [1,1,2,2,3]
+// There are two each of types 1 and 2, and one sighting of type 3. Pick the lower of the two types seen twice: type 1.
+
+// Note: It is guaranteed that each type is 1,2,3,4 or 5.
+
+// Ex:
+// Input: arr = [1, 2, 3, 4, 5, 4, 3, 2, 1, 3, 4]
+// Output: 3.

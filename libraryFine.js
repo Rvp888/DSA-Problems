fc6b2d2a7af2b1ca2ddb207737ee22d7a82daca8
Rohan Palankar <rohanpalankar8@gmail.com>
2023-03-23 21:28:35 +0530

    modify files

--- libraryFine.js
+++ libraryFine.js
@@ -6,3 +6,5 @@
 // program that calculates the fine (if any). The fee structure is as follows:
 
 // 1. If the book is returned on or before the expected return date, no fine will be charged (i.e.: fine = 0).
+// 2. If the book is returned after the expected return day but still within the same calendar month and year as 
+//    the expected return date, the fine = 15 x (the number of days late).

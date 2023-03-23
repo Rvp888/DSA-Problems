14d9d731457eb6e718bd1abb5f47ce36a822fc55
Rohan Palankar <rohanpalankar8@gmail.com>
2023-03-23 14:50:38 +0530

    modify files

--- subArrayDivision.js
+++ subArrayDivision.js
@@ -10,10 +10,32 @@
 // Determine how many ways she can divide the chocolate.
 
 // Example
-// s = [2, 2, 1, 3, 2]
+// arr = [2, 2, 1, 3, 2]
 // d = 4
 // m = 2
 
 
 // Lily wants to find segments summing to Ron's birth day, d = 4 with a length equalling his birth month, m = 2 . 
 // In this case, there are two segments meeting her criteria: [2, 2] and [1, 3].
+
+
+
+function subArrayDivision(arr, d, m) {
+    let ans = 0;
+    if(arr.length === 1 && arr[0] === d){
+        ans++;
+        return ans;
+    }
+
+    for(let i = 0; i < arr.length - m; i++){
+        let sum = 0;
+        for(let j = i; j < i + m; j++){
+            sum += arr[j];
+        }
+        if(sum === d){
+            ans++;
+        }
+    }
+
+    return ans;
+}

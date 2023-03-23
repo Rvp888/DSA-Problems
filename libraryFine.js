a8067d2e84e2b2f9e09c292fcc1097f5363d2f7e
Rohan Palankar <rohanpalankar8@gmail.com>
2023-03-23 21:31:37 +0530

    modify files

--- libraryFine.js
+++ libraryFine.js
@@ -8,3 +8,7 @@
 // 1. If the book is returned on or before the expected return date, no fine will be charged (i.e.: fine = 0).
 // 2. If the book is returned after the expected return day but still within the same calendar month and year as 
 //    the expected return date, the fine = 15 x (the number of days late).
+// 3. If the book is returned after the expected return month but still within the same calendar year as the 
+//    expected return date, the fine = 500 x (the number of months late).
+// 4. If the book is returned after the calendar year in which it was expected, there is a fixed fine of 10000.
+

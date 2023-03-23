afdba3914fe6ee0f75823b5367721e1e62d715af
Rohan Palankar <rohanpalankar8@gmail.com>
2023-03-23 22:03:27 +0530

    modify files

--- libraryFine.js
+++ libraryFine.js
@@ -12,9 +12,32 @@
 //    expected return date, the fine = 500 x (the number of months late).
 // 4. If the book is returned after the calendar year in which it was expected, there is a fixed fine of 10000.
 
-// Example:
+// Example-1:
 // d1,m1,y1 = 14,7,2018
 // d2,m2,y2 = 5,7,2018
 
 // The first values are the return date and the second are the due date. The years are the same and the months 
 // are the same. The book is 14 - 5 = 9 days late. Return 9 * 15 = 135.
+
+// Example-2:
+// d1,m1,y1 = 14,9,2018
+// d2,m2,y2 = 5,7,2018
+
+// The years are the same but the book is 9 - 7 = 2 months late. Return 2 * 500 = 1000.
+
+// Example-3:
+// d1,m1,y1 = 14,9,2019
+// d2,m2,y2 = 5,7,2018
+
+// The book is 2019 - 2018 = 1 year late. Return 10000.
+
+
+function libraryFine(d1, m1, y1, d2, m2, y2) {
+    let ans = 0;
+
+    if (y1 - y2 > 0) {
+        ans += 10000 * (y1 - y2);
+    }
+    
+}
+

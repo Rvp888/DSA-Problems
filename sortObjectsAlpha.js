77bda0d9215362c983c336d137b776997150cde5
Rohan Palankar <rohanpalankar8@gmail.com>
2023-03-27 18:44:31 +0530

    new file added

--- sortObjectsAlpha.js
+++ sortObjectsAlpha.js
@@ -2,23 +2,23 @@
 // Problem: 
 // Given an array of users (objects), sort them according to their last names. 
 
-Example:
-const users = [
-    {
-        firstName: 'Rohan',
-        lastName: 'Palankar'
-    },
-    {
-        firstName: 'Abhi',
-        lastName: 'Anvekar'
-    },
-    {
-        firstName: 'Aditya',
-        lastName: 'Revankar'
-    },
-    {
-        firstName: 'Rahul',
-        lastName: 'Bhat'
-    }
-]
+// Example:
+// const users = [
+//     {
+//         firstName: 'Rohan',
+//         lastName: 'Palankar'
+//     },
+//     {
+//         firstName: 'Abhi',
+//         lastName: 'Anvekar'
+//     },
+//     {
+//         firstName: 'Aditya',
+//         lastName: 'Revankar'
+//     },
+//     {
+//         firstName: 'Rahul',
+//         lastName: 'Bhat'
+//     }
+// ]
 

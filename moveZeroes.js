7b49a3492ecbfa18214bef1fc0d6060ed27d8eff
Rohan Palankar <rohanpalankar8@gmail.com>
2023-05-07 12:24:31 +0530

    moveZeroes algorithm updated

--- moveZeroes.js
+++ moveZeroes.js
@@ -34,4 +34,20 @@
 
 
 
+// Approach-II:
+
+const moveZeroes = function(nums) {
+
+    for (let i = nums.length-1; i >= 0; i--) {
+        if (nums[i] === 0) {
+            nums.push(0);
+            nums.splice(i, 1);
+        }
+    }
+    return nums;
+
+}
+
+console.log(moveZeroes([0,1,0,3,12]));
+
 

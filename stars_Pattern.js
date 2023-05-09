7a36f9b7959d1c6e3a0a1abb3817cdb2ac142336
Rohan Palankar <rohanpalankar8@gmail.com>
2023-05-09 22:58:39 +0530

    new file added

--- stars_Pattern.js
+++ stars_Pattern.js
@@ -1,1 +1,13 @@
 
+function stars_Pattern (n) {
+    let stars = "";
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j <= i; j++) {
+            stars = stars + "*";
+        }
+        stars = stars + "\n";
+    }
+    return stars;
+}
+
+console.log(stars_Pattern(5));

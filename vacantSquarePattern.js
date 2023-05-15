ad9d360e7f9e9bae710dc5b4cf58fb8f6588f958
Rohan Palankar <rohanpalankar8@gmail.com>
2023-05-16 00:01:31 +0530

    vacantSquarePattern.js added

---
+++ vacantSquarePattern.js
@@ -0,0 +1,33 @@
+
+// Problem: Create a vacant square pattern as shown below.
+
+// *****
+// *   *
+// *   *
+// *   *
+// *****
+
+// Solution:
+
+function vacantSquare (n) {
+
+    let pattern = "";
+    let spaces = n-2;
+
+    for (let i = 1; i <= n; i++)  {
+        for (let j = 1; j <= n; j++) {
+            if (i === 1 || i === n || j === 1 || j === n) {
+                pattern += "*";
+            }
+            else {
+                pattern += " ";
+            }
+        }
+        pattern += "\n";
+    }
+
+    return pattern;
+
+}
+
+console.log(vacantSquare(5));

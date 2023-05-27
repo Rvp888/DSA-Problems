e48f34d3791e3a210b752bf99a78a9e59e5a2910
Rohan Palankar <rohanpalankar8@gmail.com>
2023-05-27 15:36:37 +0530

    fizzBuzz.js updated

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -29,6 +29,20 @@ function fizzBuzz (n) {
 
     for (let i = 1; i <= n; i++) {
         let str = "";
+        if (i % 3 === 0) {
+            str += "Fizz";
+        }
+        if (i % 5 === 0) {
+            str += "Buzz";
+        }
+        if (i % 3 !== 0 && i % 5 !== 0) {
+            str += i;
+        }
+        result.push(str);
     }
 
+    return result;
+
 }
+
+console.log(fizzBuzz(15));

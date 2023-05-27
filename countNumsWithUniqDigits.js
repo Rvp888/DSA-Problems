5bb4969148cad206374c576f386d6a4263de1567
Rohan Palankar <rohanpalankar8@gmail.com>
2023-05-27 15:47:26 +0530

    countNumsWithUniqDigits.js updated

--- countNumsWithUniqDigits.js
+++ countNumsWithUniqDigits.js
@@ -21,4 +21,11 @@ function countNumbersWithUniqueDigits (n) {
         return 1;
     }
 
+    let ans = 10, base = 9;
+
+    for (let i = 2; i <= n; i++) {
+        base *= (9 - i + 2);
+        ans += base;
+    }
+
 }

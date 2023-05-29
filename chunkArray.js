bf8b6d9beb5b9ee89c6e2afcc5a6b84b1adfd91f
Rohan Palankar <rohanpalankar8@gmail.com>
2023-05-29 14:35:29 +0530

    new file chunkArray.js added

--- chunkArray.js
+++ chunkArray.js
@@ -8,3 +8,13 @@
 
 // Please solve it without using lodash's _.chunk function.
 
+// Example 1:
+// Input: arr = [1,2,3,4,5], size = 1
+// Output: [[1],[2],[3],[4],[5]]
+// Explanation: The arr has been split into subarrays each with 1 element.
+
+// Example 2:
+// Input: arr = [1,9,6,3,2], size = 3
+// Output: [[1,9,6],[3,2]]
+// Explanation: The arr has been split into subarrays with 3 elements. However, only two elements are left for the 2nd subarray.
+

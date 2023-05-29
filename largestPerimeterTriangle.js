0b8312878a8b860997ff75d67991ed311257187e
Rohan Palankar <rohanpalankar8@gmail.com>
2023-05-30 00:01:53 +0530

    largestPerimeterTriangle.js updated

--- largestPerimeterTriangle.js
+++ largestPerimeterTriangle.js
@@ -24,3 +24,16 @@
 // The sum of lengths of two sides should always be greater than the length of the third side.
 
 
+var largestPerimeter = function(nums) {
+    nums = nums.sort((a,b) => b-a);
+
+    for (let i = 0; i <  nums.length - 2; i++) {
+        if (nums[i] < (nums[i+1]+nums[i+2])) {
+            return nums[i]+nums[i+1]+nums[i+2];
+        }
+    }
+    
+    return 0;
+};
+
+console.log(largestPerimeter([1,2,1,10]));

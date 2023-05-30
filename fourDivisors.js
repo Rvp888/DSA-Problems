1969d5a05eddf5686905b0d2901c91686e9df726
Rohan Palankar <rohanpalankar8@gmail.com>
2023-05-31 00:48:55 +0530

    fourDivisors.js updated

--- fourDivisors.js
+++ fourDivisors.js
@@ -19,3 +19,40 @@
 // Example 3:
 // Input: nums = [1,2,3,4,5]
 // Output: 0
+
+// Leetcode link: https://leetcode.com/problems/four-divisors/
+
+
+// Solution:
+
+var sumFourDivisors = function(nums) {
+    let result = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        let count = 2;
+        let sum = 1+nums[i];
+        let j = 2;
+        let k = nums[i];
+        while ( j < k ) {
+            if (nums[i] % j === 0) {
+                k = nums[i] / j
+                if (k === j) {
+                    sum = sum + j;
+                    count += 1;
+                } 
+                else {
+                    sum = sum + j + k;
+                    count += 2;
+                }
+            }
+            j++;
+            if (count > 4) break;
+        }
+        if (count === 4) {
+            result += sum;
+        }
+    }
+
+    return result;
+};
+

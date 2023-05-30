5be20d4b8acb078bbb3e8e7a94ab7432ba732275
Rohan Palankar <rohanpalankar8@gmail.com>
2023-05-30 11:47:30 +0530

    distributeCandies.js updated

--- distributeCandies.js
+++ distributeCandies.js
@@ -30,6 +30,7 @@
 // On the third turn, ans[2] += 3, and the array is [1,2,3].
 // On the fourth turn, ans[0] += 4, and the final array is [5,2,3].
 
+// Leetcode link-  https://leetcode.com/problems/distribute-candies-to-people/description/
 
 // Solution:
 

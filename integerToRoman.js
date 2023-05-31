e000e9e516299d7ae5d438047be5dc03a098cc03
Rohan Palankar <rohanpalankar8@gmail.com>
2023-05-31 23:55:19 +0530

    integerToRoman.js updated

--- integerToRoman.js
+++ integerToRoman.js
@@ -34,3 +34,90 @@
 
 // Solution:
 
+var intToRoman = function(num) {
+    let result = "";
+    if (num / 1000 >= 1) {
+        let thousands = Math.floor(num/1000);
+        while(thousands) {
+            result += "M";
+            thousands--;
+        }
+    }
+    num = num % 1000;
+    if (num / 100 >= 1) {
+        let hundreds = Math.floor(num/100);
+        if (hundreds === 5) result += "D";
+        if (hundreds < 5) {
+            if (hundreds === 4) result += "CD";
+            else {
+                while(hundreds) {
+                    result += "C";
+                    hundreds--;
+                }
+            }
+        }
+        if (hundreds > 5) {
+            if (hundreds === 9) result += "CM";
+            else {
+                result += "D";
+                hundreds = hundreds - 5;
+                while(hundreds) {
+                    result += "C";
+                    hundreds--;
+                }
+            }
+        }
+    }
+    num = num % 100;
+    if (num / 10 >= 1) {
+        let tens = Math.floor(num/10);
+        if (tens === 5) result += "L";
+        if (tens < 5) {
+            if (tens === 4) result += "XL";
+            else {
+                while(tens) {
+                    result += "X";
+                    tens--;
+                }
+            }
+        }
+        if (tens > 5) {
+            if (tens === 9) result += "XC";
+            else {
+                result += "L";
+                tens = tens - 5;
+                while(tens) {
+                    result += "X";
+                    tens--;
+                }
+            }
+        }
+    }
+    num = num % 10;
+    if (num / 1 >= 1) {
+        let ones = Math.floor(num/1);
+        if (ones === 5) result += "V";
+        if (ones < 5) {
+            if (ones === 4) result += "IV";
+            else {
+                while(ones) {
+                    result += "I";
+                    ones--;
+                }
+            }
+        }
+        if (ones > 5) {
+            if (ones === 9) result += "IX";
+            else {
+                result += "V";
+                ones = ones - 5;
+                while(ones) {
+                    result += "I";
+                    ones--;
+                }
+            }
+        }
+    }
+
+    return result;
+};

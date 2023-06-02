1ee9b27343fd684fa7f8deee7848fb3aced5231d
Rohan Palankar <rohanpalankar8@gmail.com>
2023-06-02 12:10:16 +0530

    nonDuplicateElement.js updated

--- nonDuplicateElement.js
+++ nonDuplicateElement.js
@@ -16,3 +16,29 @@
 // Input: nums = [3,3,7,7,10,11,11]
 // Output: 10
 
+
+var singleNonDuplicate = function(nums) {
+    let left = 0, right = nums.length-1;
+    while (left <= right) {
+        let mid = left + Math.floor((right-left)/2);
+        if (nums[mid-1] === nums[mid]) {
+            if ((mid-1) % 2) {
+                right = mid-2;
+            }
+            else {
+                left = mid+1;
+            }
+        }
+        else if (nums[mid+1] === nums[mid]) {
+            if ((mid+1) % 2) {
+                left = mid+2;
+            }
+            else {
+                right = mid-1;
+            }
+        }
+        else {
+            return nums[mid];
+        }
+    }
+};

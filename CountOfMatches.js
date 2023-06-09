0d904837c95308c66801ef677c5feee014509797
Rohan Palankar <rohanpalankar8@gmail.com>
2023-06-09 23:58:45 +0530

    file countOfMatches.js updated

--- CountOfMatches.js
+++ CountOfMatches.js
@@ -28,3 +28,22 @@
 // - 4th Round: Teams = 2, Matches = 1, and 1 team is declared the winner.
 // Total number of matches = 7 + 3 + 2 + 1 = 13.
 
+// Solution:
+
+var numberOfMatches = function(n) {
+    let matches = 0;
+
+    while (n > 1) {
+        if (n % 2 === 0) {
+            matches += n/2;
+            n = n/2;
+        }
+        else {
+            matches += (n-1)/2;
+            n = (n-1)/2 + 1;
+        }
+    }
+
+    return matches;
+};
+

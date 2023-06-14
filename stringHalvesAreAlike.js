7b6c1c6f59e30f8f7c94d6692c7c8689c2a6ecd8
Rohan Palankar <rohanpalankar8@gmail.com>
2023-06-14 22:51:53 +0530

    file stringHalvesAreAlike.js updated

--- stringHalvesAreAlike.js
+++ stringHalvesAreAlike.js
@@ -19,3 +19,28 @@
 // Output: false
 // Explanation: a = "text" and b = "book". a has 1 vowel whereas b has 2. Therefore, they are not alike.
 // Notice that the vowel o is counted twice.
+
+// Solution:
+
+var halvesAreAlike = function(s) {
+
+    let vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+    let len = s.length;
+    let firstHalf = s.substr(0, len/2);
+    let secondHalf = s.substr(len/2, len/2);
+
+    let vowelsCount1 = 0, vowelsCount2 = 0;
+
+    for (let i = 0; i < len/2; i++) {
+        if (vowels.includes(firstHalf[i])) {
+            vowelsCount1++;
+        }
+        if (vowels.includes(secondHalf[i])) {
+            vowelsCount2++;
+        }
+    }
+
+    return vowelsCount1 === vowelsCount2;
+};
+
+

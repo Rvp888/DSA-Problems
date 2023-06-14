eab46495a7c579306f7be4d6c73e967f30602c8c
Rohan Palankar <rohanpalankar8@gmail.com>
2023-06-14 20:51:03 +0530

    file reverseDigitsWithChars.js updated

--- reverseDigitsWithChars.js
+++ reverseDigitsWithChars.js
@@ -10,3 +10,20 @@
 
 // Return s after replacing all digits. It is guaranteed that shift(s[i-1], s[i]) will never exceed 'z'.
 
+// Example 1:
+// Input: s = "a1c1e1"
+// Output: "abcdef"
+// Explanation: The digits are replaced as follows:
+// - s[1] -> shift('a',1) = 'b'
+// - s[3] -> shift('c',1) = 'd'
+// - s[5] -> shift('e',1) = 'f'
+
+// Example 2:
+// Input: s = "a1b2c3d4e"
+// Output: "abbdcfdhe"
+// Explanation: The digits are replaced as follows:
+// - s[1] -> shift('a',1) = 'b'
+// - s[3] -> shift('b',2) = 'd'
+// - s[5] -> shift('c',3) = 'f'
+// - s[7] -> shift('d',4) = 'h'
+

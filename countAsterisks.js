9e08eb815ba5c32bb4c0c664ad0d8857cc391033
Rohan Palankar <rohanpalankar8@gmail.com>
2023-06-14 19:37:48 +0530

    file countAsterisks.js updated

--- countAsterisks.js
+++ countAsterisks.js
@@ -1,7 +1,6 @@
-
 // Count Asterisks:
 
-// You are given a string s, where every two consecutive vertical bars '|' are grouped into a pair. 
+// You are given a string s, where every two consecutive vertical bars '|' are grouped into a pair.
 // In other words, the 1st and 2nd '|' make a pair, the 3rd and 4th '|' make a pair, and so forth.
 
 // Return the number of '*' in s, excluding the '*' between each pair of '|'.
@@ -26,3 +25,21 @@
 // Output: 5
 // Explanation: The considered characters are underlined: "yo|uar|e**|b|e***au|tifu|l". There are 5 asterisks considered. Therefore, we return 5.
 
+// Solution:
+
+var countAsterisks = function (s) {
+  let count = 0,
+    tempCount = 0,
+    countOfBars = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] == "|") countOfBars++;
+    if (s[i] == "*") {
+      if (countOfBars % 2 == 0) count++;
+      else tempCount++;
+    }
+    if (countOfBars % 2 == 0) tempCount = 0;
+  }
+
+  return count + tempCount;
+};

f6e583c4ca0af94bc2bc243eeaaf8ad14cd79846
Rohan Palankar <rohanpalankar8@gmail.com>
2023-06-15 00:53:50 +0530

    file equalOccurrences.js updated

--- equalOccurrences.js
+++ equalOccurrences.js
@@ -18,3 +18,29 @@
 // 'a' occurs 3 times while 'b' occurs 2 times, which is not the same number of times.
 
 
+// Solution:
+
+var areOccurrencesEqual = function(s) {
+    
+    let obj = {};
+
+    for (let chr of s) {
+        if (obj[chr]) {
+            obj[chr]++;
+        }
+        else {
+            obj[chr] = 1;
+        }
+    }
+
+    let values = Object.values(obj);
+    console.log(values);
+
+    for (let i = 0; i < values.length-1; i++) {
+        if (values[i] != values[i+1]) {
+            return false;
+        }
+    }
+
+    return true;
+};

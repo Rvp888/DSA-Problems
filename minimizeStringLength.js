8d4197087d1ce71f69adcd4b0de0b6d613226022
Rohan Palankar <rohanpalankar8@gmail.com>
2023-06-15 15:11:27 +0530

    file minimizeStringLength.js updated

--- minimizeStringLength.js
+++ minimizeStringLength.js
@@ -9,4 +9,19 @@
 
 // Return an integer denoting the length of the minimized string.
 
+// Example 1:
+// Input: s = "aaabc"
+// Output: 3
+// Explanation: In this example, s is "aaabc". We can start by selecting the character 'a' at index 1. 
+// We then remove the closest 'a' to the left of index 1, which is at index 0, and the closest 'a' to 
+// the right of index 1, which is at index 2. After this operation, the string becomes "abc". Any further 
+// operation we perform on the string will leave it unchanged. Therefore, the length of the minimized string is 3.
+
+// Example 2:
+// Input: s = "cbbd"
+// Output: 3
+// Explanation: For this we can start with character 'b' at index 1. There is no occurrence of 'b' to 
+// the left of index 1, but there is one to the right at index 2, so we delete the 'b' at index 2. 
+// The string becomes "cbd" and further operations will leave it unchanged. Hence, the minimized length is 3.
+
 

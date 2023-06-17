12f80e8cd7271eb7b04fcb644f4490d4473f4c5b
Rohan Palankar <rohanpalankar8@gmail.com>
2023-06-17 16:27:13 +0530

    file findWordsInKeyboardRow.js updated

--- findWordsInKeyboardRow.js
+++ findWordsInKeyboardRow.js
@@ -23,3 +23,55 @@
 
 // Input: words = ["adsdf","sfd"]
 // Output: ["adsdf","sfd"]
+
+// Leetcode link: https://leetcode.com/problems/keyboard-row/description/
+
+var findWords = function(words) {
+    
+    let result = [];
+    let row1 = 'qwertyuiopQWERTYUIOP';
+    let row2 = 'asdfghjklASDFGHJKL';
+    let row3 = 'zxcvbnmZXCVBNM';
+
+    for (let i = 0; i < words.length; i++) {
+        
+        let word = words[i];
+        let canType = true;
+        for (let j = 0; j < word.length; j++) {
+            if (!row1.includes(word[j])) {
+                canType = false;
+                break;
+            }
+        }
+        if (canType) {
+            result.push(word);
+            continue;
+        } 
+        canType = true;
+        for (let j = 0; j < word.length; j++) {
+            if (!row2.includes(word[j])) {
+                canType = false;
+                break;
+            }
+        }
+        if (canType) {
+            result.push(word);
+            continue;
+        }
+        canType = true;
+        for (let j = 0; j < word.length; j++) {
+            if (!row3.includes(word[j])) {
+                canType = false;
+                break;
+            }
+        }
+        if (canType) {
+            result.push(word);
+            continue;
+        }
+    }
+
+    return result;
+
+};
+

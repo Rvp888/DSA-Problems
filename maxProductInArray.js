e56fd30dd32ad4140d23b925569ffd3430ce46cc
Rohan Palankar <rohanpalankar8@gmail.com>
2023-06-18 14:57:49 +0530

    file maxProductInArray.js updated

--- maxProductInArray.js
+++ maxProductInArray.js
@@ -15,3 +15,23 @@
 // Input: nums = [1,5,4,5]
 // Output: 16
 // Explanation: Choosing the indices i=1 and j=3 (indexed from 0), you will get the maximum value of (5-1)*(5-1) = 16.
+
+// Leetcode link: https://leetcode.com/problems/maximum-product-of-two-elements-in-an-array/description/
+
+// Solution:
+
+var maxProduct = function(nums) {
+    
+    let result = (nums[0]-1)*(nums[1]-1);
+
+    for (let i = 0; i < nums.length-1; i++) {
+        for (let j = i+1; j < nums.length; j++) {
+            if ((nums[i]-1)*(nums[j]-1) > result) {
+                result = (nums[i]-1)*(nums[j]-1);
+            }
+        }
+    }
+
+    return result;
+
+};

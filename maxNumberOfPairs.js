10fb10c86022b819d843c42dbc0dcf53d38a7928
Rohan Palankar <rohanpalankar8@gmail.com>
2023-06-20 20:57:35 +0530

    file maxNumberOfPairs.js updated

--- maxNumberOfPairs.js
+++ maxNumberOfPairs.js
@@ -11,4 +11,14 @@
 // that are formed and answer[1] is the number of leftover integers in nums after doing the 
 // operation as many times as possible.
 
+// Example 1:
+
+// Input: nums = [1,3,2,1,3,2,2]
+// Output: [3,1]
+// Explanation:
+// Form a pair with nums[0] and nums[3] and remove them from nums. Now, nums = [3,2,3,2,2].
+// Form a pair with nums[0] and nums[2] and remove them from nums. Now, nums = [2,2,2].
+// Form a pair with nums[0] and nums[1] and remove them from nums. Now, nums = [2].
+// No more pairs can be formed. A total of 3 pairs have been formed, and there is 1 number leftover in nums.
+
 

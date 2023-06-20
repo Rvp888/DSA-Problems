496e949bf039b5be4ba634c458c41b0f31f1882e
Rohan Palankar <rohanpalankar8@gmail.com>
2023-06-20 21:31:41 +0530

    file mergeSimilarItems.js updated

--- mergeSimilarItems.js
+++ mergeSimilarItems.js
@@ -45,3 +45,5 @@ var mergeSimilarItems = function(items1, items2) {
     return Object.entries(collection);
 
 };
+
+console.log(mergeSimilarItems(items1 = [[1,1],[4,5],[3,8]], items2 = [[3,1],[1,5]]));

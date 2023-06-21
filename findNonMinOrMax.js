228342e676894da8bcfe40b59cbfbfddf0f4306a
Rohan Palankar <rohanpalankar8@gmail.com>
2023-06-21 17:41:32 +0530

    file findNonMinOrMax.js updated

--- findNonMinOrMax.js
+++ findNonMinOrMax.js
@@ -6,3 +6,18 @@
 
 // Return the selected integer.
 
+// Example 1:
+
+// Input: nums = [3,2,1,4]
+// Output: 2
+// Explanation: In this example, the minimum value is 1 and the maximum value is 4. 
+// Therefore, either 2 or 3 can be valid answers.
+
+// Example 2:
+
+// Input: nums = [1,2]
+// Output: -1
+// Explanation: Since there is no number in nums that is neither the maximum nor the 
+// minimum, we cannot select a number that satisfies the given condition. Therefore, 
+// there is no answer.
+

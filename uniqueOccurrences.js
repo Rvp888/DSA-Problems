d94fed47a3da743e60254aad550d7e38e5f2c956
Rohan Palankar <rohanpalankar8@gmail.com>
2023-06-21 12:14:01 +0530

    file uniqueOccurrences.js updated

--- uniqueOccurrences.js
+++ uniqueOccurrences.js
@@ -14,3 +14,63 @@
 // Input: arr = [1,2]
 // Output: false
 
+
+// Leetcode link: https://leetcode.com/problems/unique-number-of-occurrences/description/
+
+
+// Solution-1:
+// var uniqueOccurrences = function(arr) {
+
+//    let obj = {};
+
+//    for (let i = 0; i < arr.length; i++) {
+//        if (obj[arr[i]]) {
+//            obj[arr[i]]++;
+//        }
+//        else {
+//            obj[arr[i]] = 1;
+//        }
+//    }
+
+//    let occurrences = Object.values(obj).sort((a,b) => a-b);
+
+//    for (let i = 0; i < occurrences.length-1; i++) {
+//        if (occurrences[i] === occurrences[i+1]) {
+//            return false;
+//        }
+//    }
+
+//    return true;
+
+// };
+
+
+// Solution-2:
+var uniqueOccurrences = function(arr) {
+
+    let obj1 = {}, obj2 = {};
+ 
+    for (let i = 0; i < arr.length; i++) {
+        if (obj1[arr[i]]) {
+            obj1[arr[i]]++;
+        }
+        else {
+            obj1[arr[i]] = 1;
+        }
+    }
+ 
+    for (let key in obj1) {
+        if (obj2[obj1[key]]) {
+            return false;
+        }
+        else {
+            obj2[obj1[key]] = true;
+        }
+    }
+ 
+    return true; 
+ 
+ };
+
+ 
+

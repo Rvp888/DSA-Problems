e601d9fd70d859eedbafb70dad88525a075b54ee
Rohan Palankar <rohanpalankar8@gmail.com>
2023-06-23 00:02:55 +0530

    file canBeEqual.js updated

--- canBeEqual.js
+++ canBeEqual.js
@@ -15,3 +15,22 @@
 // 3- Reverse subarray [4,3], arr becomes [1,2,3,4]
 // There are multiple ways to convert arr to target, this is not the only way to do so.
 
+// Leetcode link: https://leetcode.com/problems/make-two-arrays-equal-by-reversing-subarrays/description/
+
+// Solution:
+
+var canBeEqual = function(target, arr) {
+    
+    for (let i = 0; i < arr.length; i++) {
+        if (!target.includes(arr[i])) {
+            return false;
+        }
+        else {
+            let index = target.indexOf(arr[i]);
+            target.splice(index, 1);
+        }
+    }
+
+    return true;
+
+};

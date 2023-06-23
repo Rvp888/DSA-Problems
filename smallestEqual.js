269c24d7a98f3732d6e935171f98f4fa1fcfbf57
Rohan Palankar <rohanpalankar8@gmail.com>
2023-06-23 17:07:10 +0530

    file smallestEqual.js updated

--- smallestEqual.js
+++ smallestEqual.js
@@ -32,3 +32,18 @@
 // Output: -1
 // Explanation: No index satisfies i mod 10 == nums[i].
 
+// Leetcode link: https://leetcode.com/problems/smallest-index-with-equal-value/description/
+
+// Solution:
+
+var smallestEqual = function(nums) {
+
+    for (let i = 0; i < nums.length; i++) {
+        if (i % 10 === nums[i]) {
+            return i;
+        }
+    }
+
+    return -1;
+
+};

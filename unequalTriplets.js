cff97277a049fabab55979f275ad23965092c33c
Rohan Palankar <rohanpalankar8@gmail.com>
2023-06-24 15:58:11 +0530

    file unequalTriplets.js updated

--- unequalTriplets.js
+++ unequalTriplets.js
@@ -23,3 +23,30 @@
 
 // Leetcode link: https://leetcode.com/problems/number-of-unequal-triplets-in-array/description/
 
+
+// Solution:
+
+var unequalTriplets = function(nums) {
+    
+    let tripletsCount = 0;
+
+    for (let i = 0; i < nums.length-2; i++) {
+
+        for (let j = i+1; j < nums.length-1; j++) {
+
+            if (nums[i] != nums[j]) {
+                
+                for (let k = j+1; k < nums.length; k++) {
+
+                    if (nums[i] != nums[k] && nums[j] != nums[k]) {
+                        tripletsCount++;
+                    }
+                }
+            }
+        }
+    }
+
+    return tripletsCount;
+
+};
+

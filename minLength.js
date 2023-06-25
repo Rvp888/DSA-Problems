9b38eaca39cade86df96e182cf01e16f183d2c66
Rohan Palankar <rohanpalankar8@gmail.com>
2023-06-25 18:15:07 +0530

    file minLength.js updated

--- minLength.js
+++ minLength.js
@@ -10,4 +10,20 @@
 // Note that the string concatenates after removing the substring and could produce 
 // new "AB" or "CD" substrings.
 
+// Example 1:
+
+// Input: s = "ABFCACDB"
+// Output: 2
+// Explanation: We can do the following operations:
+// - Remove the substring "ABFCACDB", so s = "FCACDB".
+// - Remove the substring "FCACDB", so s = "FCAB".
+// - Remove the substring "FCAB", so s = "FC".
+// So the resulting length of the string is 2.
+// It can be shown that it is the minimum length that we can obtain.
+
+// Example 2:
+
+// Input: s = "ACBBD"
+// Output: 5
+// Explanation: We cannot do any operations on the string so the length remains the same.
 

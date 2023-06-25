aa50ed594fc425d0638490394a328e5846b255c5
Rohan Palankar <rohanpalankar8@gmail.com>
2023-06-25 18:57:59 +0530

    file commonChars.js updated

--- commonChars.js
+++ commonChars.js
@@ -17,3 +17,28 @@
 
 // Solution:
 
+var commonChars = function(words) {
+    
+    let result = [];
+
+    for (let i = 0; i < words[0].length; i++) {
+        let char = words[0][i];
+        let includes = true;
+        for (let j = 1; j < words.length; j++) {
+            if (!words[j].includes(char)) {
+                includes = false;
+            }
+            else {
+                words[j] = words[j].split("");
+                let index = words[j].indexOf(char);
+                words[j].splice(index, 1);
+                words[j] = words[j].join("");
+            }
+        }
+        if (includes) result.push(char);
+    }
+
+    return result;
+
+};
+

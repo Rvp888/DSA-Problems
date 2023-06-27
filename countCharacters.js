d4972e24d1bc6e46217509b2157681df06ce297a
Rohan Palankar <rohanpalankar8@gmail.com>
2023-06-27 14:22:13 +0530

    file countCharacters.js updated

--- countCharacters.js
+++ countCharacters.js
@@ -6,4 +6,9 @@
 
 // Return the sum of lengths of all good strings in words.
 
+// Example:
+
+// Input: words = ["cat","bt","hat","tree"], chars = "atach"
+// Output: 6
+// Explanation: The strings that can be formed are "cat" and "hat" so the answer is 3 + 3 = 6.
 

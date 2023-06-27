9bf96d9f94472890b22c676cdd9efc121cd59763
Rohan Palankar <rohanpalankar8@gmail.com>
2023-06-27 16:44:21 +0530

    file numUniqueEmails.js updated

--- numUniqueEmails.js
+++ numUniqueEmails.js
@@ -21,6 +21,18 @@
 // Given an array of strings emails where we send one email to each emails[i], return the 
 // number of different addresses that actually receive mails.
 
+// Example 1:
+
+// Input: emails = ["test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"]
+// Output: 2
+// Explanation: "testemail@leetcode.com" and "testemail@lee.tcode.com" actually receive mails.
+
+// Example 2:
+
+// Input: emails = ["a@leetcode.com","b@leetcode.com","c@leetcode.com"]
+// Output: 3
+
+
 // Leetcode link: https://leetcode.com/problems/unique-email-addresses/description/
 
 // Solution: 
@@ -59,4 +71,5 @@ var numUniqueEmails = function(emails) {
     return newEmails.length;
 
 };
+
 

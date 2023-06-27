8c347d399a387d09a5fb1f8d711eac9d2dbd630b
Rohan Palankar <rohanpalankar8@gmail.com>
2023-06-27 14:23:13 +0530

    file countCharacters.js updated

--- countCharacters.js
+++ countCharacters.js
@@ -12,3 +12,33 @@
 // Output: 6
 // Explanation: The strings that can be formed are "cat" and "hat" so the answer is 3 + 3 = 6.
 
+// Leetcode link: https://leetcode.com/problems/find-words-that-can-be-formed-by-characters/description/
+
+// Solution:
+
+var countCharacters = function(words, chars) {
+    
+    let sumOfLengths = 0;
+
+    for (let i = 0; i < words.length; i++) {
+        let tempChars = chars.split("");
+        let word = words[i];
+        let canForm = true;
+        for (let j = 0; j < word.length; j++) {
+            if (!tempChars.includes(word[j])) {
+                canForm = false;
+            }
+            else {
+                let index = tempChars.indexOf(word[j]);
+                tempChars.splice(index, 1);
+            }
+        }
+        if (canForm) {
+            sumOfLengths += word.length;
+        }
+    }
+
+    return sumOfLengths;
+
+};
+

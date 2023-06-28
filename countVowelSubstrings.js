506600b1016ed7cb701f7a55500b747af5b1ce75
Rohan Palankar <rohanpalankar8@gmail.com>
2023-06-28 12:02:05 +0530

    file countVowelSubstrings.js updated

--- countVowelSubstrings.js
+++ countVowelSubstrings.js
@@ -7,3 +7,24 @@
 
 // Given a string word, return the number of vowel substrings in word.
 
+// Example 1:
+
+// Input: word = "aeiouu"
+// Output: 2
+// Explanation: The vowel substrings of word are as follows (underlined):
+// - "aeiouu"
+// - "aeiouu"
+
+// Example 2:
+
+// Input: word = "cuaieuouac"
+// Output: 7
+// Explanation: The vowel substrings of word are as follows (underlined):
+// - "cuaieuouac"
+// - "cuaieuouac"
+// - "cuaieuouac"
+// - "cuaieuouac"
+// - "cuaieuouac"
+// - "cuaieuouac"
+// - "cuaieuouac"
+

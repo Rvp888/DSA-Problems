3af8aacd955d52f3140581461d491f07952ec276
Rohan Palankar <rohanpalankar8@gmail.com>
2023-06-28 18:33:43 +0530

    file digitSum.js updated

--- digitSum.js
+++ digitSum.js
@@ -15,3 +15,16 @@
 
 // Return s after all rounds have been completed.
 
+// Example 1:
+
+// Input: s = "11111222223", k = 3
+// Output: "135"
+// Explanation: 
+// - For the first round, we divide s into groups of size 3: "111", "112", "222", and "23".
+//   ​​​​​Then we calculate the digit sum of each group: 1 + 1 + 1 = 3, 1 + 1 + 2 = 4, 2 + 2 + 2 = 6, and 2 + 3 = 5. 
+//   So, s becomes "3" + "4" + "6" + "5" = "3465" after the first round.
+// - For the second round, we divide s into "346" and "5".
+//   Then we calculate the digit sum of each group: 3 + 4 + 6 = 13, 5 = 5. 
+//   So, s becomes "13" + "5" = "135" after second round. 
+// Now, s.length <= k, so we return "135" as the answer.
+

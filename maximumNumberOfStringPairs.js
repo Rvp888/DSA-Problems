4f12de27866f044edc620758dc3ca1bfbabe4f5e
Rohan Palankar <rohanpalankar8@gmail.com>
2023-06-29 15:18:48 +0530

    file maximumNumberOfStringPairs.js updated

--- maximumNumberOfStringPairs.js
+++ maximumNumberOfStringPairs.js
@@ -11,4 +11,13 @@
 
 // Note that each string can belong in at most one pair.
 
+// Example:
+
+// Input: words = ["cd","ac","dc","ca","zz"]
+// Output: 2
+// Explanation: In this example, we can form 2 pair of strings in the following way:
+// - We pair the 0th string with the 2nd string, as the reversed string of word[0] is "dc" and is equal to words[2].
+// - We pair the 1st string with the 3rd string, as the reversed string of word[1] is "ca" and is equal to words[3].
+// It can be proven that 2 is the maximum number of pairs that can be formed.
+
 

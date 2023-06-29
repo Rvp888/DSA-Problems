c67821db899922dda04f16d47cbfc76a7174db07
Rohan Palankar <rohanpalankar8@gmail.com>
2023-06-29 15:19:53 +0530

    file maximumNumberOfStringPairs.js updated

--- maximumNumberOfStringPairs.js
+++ maximumNumberOfStringPairs.js
@@ -20,4 +20,29 @@
 // - We pair the 1st string with the 3rd string, as the reversed string of word[1] is "ca" and is equal to words[3].
 // It can be proven that 2 is the maximum number of pairs that can be formed.
 
+// Leetcode link: https://leetcode.com/problems/find-maximum-number-of-string-pairs/description/
+
+// Solution:
+
+var maximumNumberOfStringPairs = function(words) {
+    
+    let count = 0;
+
+    for (let i = 0; i < words.length-1; i++) {
+        let str1 = words[i];
+        for (let j = i+1; j < words.length; j++) {
+            let str2 = words[j];
+            str2 = str2.split("");
+            str2 = str2.reverse();
+            str2 = str2.join("");
+            if (str1 === str2) {
+                count++;
+            }
+        }
+    }
+
+    return count;
+
+};
+
 

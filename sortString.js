0126ff8df9d3c88a394afe7da40ecf482a56b644
Rohan Palankar <rohanpalankar8@gmail.com>
2023-06-30 17:25:33 +0530

    file sortString.js updated

--- sortString.js
+++ sortString.js
@@ -13,3 +13,19 @@
 
 // Return the result string after sorting s with this algorithm.
 
+// Example 1:
+
+// Input: s = "aaaabbbbcccc"
+// Output: "abccbaabccba"
+// Explanation: After steps 1, 2 and 3 of the first iteration, result = "abc"
+// After steps 4, 5 and 6 of the first iteration, result = "abccba"
+// First iteration is done. Now s = "aabbcc" and we go back to step 1
+// After steps 1, 2 and 3 of the second iteration, result = "abccbaabc"
+// After steps 4, 5 and 6 of the second iteration, result = "abccbaabccba"
+
+// Example 2:
+
+// Input: s = "rat"
+// Output: "art"
+// Explanation: The word "rat" becomes "art" after re-ordering it with the mentioned algorithm.
+

8093362e9e501d13b23d4232042f81fad29f8954
Rohan Palankar <rohanpalankar8@gmail.com>
2023-06-30 09:57:45 +0530

    file countBalls.js updated

--- countBalls.js
+++ countBalls.js
@@ -10,4 +10,22 @@
 
 // Given two integers lowLimit and highLimit, return the number of balls in the box with the most balls.
 
+// Example 1:
+
+// Input: lowLimit = 1, highLimit = 10
+// Output: 2
+// Explanation:
+// Box Number:  1 2 3 4 5 6 7 8 9 10 11 ...
+// Ball Count:  2 1 1 1 1 1 1 1 1 0  0  ...
+// Box 1 has the most number of balls with 2 balls.
+
+// Example 2:
+
+// Input: lowLimit = 5, highLimit = 15
+// Output: 2
+// Explanation:
+// Box Number:  1 2 3 4 5 6 7 8 9 10 11 ...
+// Ball Count:  1 1 1 1 2 2 1 1 1 0  0  ...
+// Boxes 5 and 6 have the most number of balls with 2 balls in each.
+
 

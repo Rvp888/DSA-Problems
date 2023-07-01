072536ef8a94be586e1a9b0094708f9f393fd389
Rohan Palankar <rohanpalankar8@gmail.com>
2023-07-01 12:51:36 +0530

    file minimumSum.js updated

--- minimumSum.js
+++ minimumSum.js
@@ -9,3 +9,17 @@
 
 // Return the minimum possible sum of new1 and new2.
 
+// Example 1:
+
+// Input: num = 2932
+// Output: 52
+// Explanation: Some possible pairs [new1, new2] are [29, 23], [223, 9], etc.
+// The minimum sum can be obtained by the pair [29, 23]: 29 + 23 = 52.
+
+// Example 2:
+
+// Input: num = 4009
+// Output: 13
+// Explanation: Some possible pairs [new1, new2] are [0, 49], [490, 0], etc. 
+// The minimum sum can be obtained by the pair [4, 9]: 4 + 9 = 13.
+

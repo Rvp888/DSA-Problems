095a19a0c2c278b5e51dbe111c3c12d35c855236
Rohan Palankar <rohanpalankar8@gmail.com>
2023-07-03 16:04:25 +0530

    file countLargestGroup.js updated

--- countLargestGroup.js
+++ countLargestGroup.js
@@ -20,4 +20,44 @@
 // Output: 2
 // Explanation: There are 2 groups [1], [2] of size 1.
 
+// Leetcode link: https://leetcode.com/problems/count-largest-group/description/
+
+// Solution:
+
+var countLargestGroup = function(n) {
+    
+    let obj  ={};
+
+    for (let i = 1; i <= n; i++) {
+        let num = i;
+        let newNum = 0;
+        if (num < 10) newNum = num;
+        else {
+            num = num.toString();
+            for (let j = 0; j < num.length; j++) {
+                newNum += Number(num[j]);
+            }
+        }
+        if (obj[newNum]) {
+            obj[newNum]++;
+        }
+        else {
+            obj[newNum] = 1;
+        }
+    }
+
+    let values = Object.values(obj);
+    let largest = Math.max(...values);
+    let count = 0;
+
+    for (let i = 0; i < values.length; i++) {
+        if (values[i] === largest) {
+            count++;
+        }
+    }
+
+    return count;
+
+};
+
 

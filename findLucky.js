3eadc72d470004b747c72f312dd46a135efc2f27
Rohan Palankar <rohanpalankar8@gmail.com>
2023-07-05 21:07:30 +0530

    file findLucky.js updated

--- findLucky.js
+++ findLucky.js
@@ -25,39 +25,9 @@
 // Leetcode link: https://leetcode.com/problems/find-lucky-integer-in-an-array/description/
 
 // Solution-1:
-// var findLucky = function(arr) {
-    
-//     let lucky = -1;
-//     let obj = {};
-
-//     for (let i = 0; i < arr.length; i++) {
-//         if (obj[arr[i]]) {
-//             obj[arr[i]]++;
-//         }
-//         else {
-//             obj[arr[i]] = 1;
-//         }
-//     }
-
-//     for (let key in obj) {
-//         if (obj[key] == key) {
-//             lucky = key;
-//         }
-//     }
-
-//     return lucky;
-
-// };
-
-// TC => O(n);
-// SC => O(n);
-
-// console.log(findLucky(arr = [1,2,2,3,3,3]));
-
-
-// Solution-2:
 var findLucky = function(arr) {
     
+    let lucky = -1;
     let obj = {};
 
     for (let i = 0; i < arr.length; i++) {
@@ -69,22 +39,52 @@ var findLucky = function(arr) {
         }
     }
 
-    let keys = Object.keys(obj);
-    keys.sort((a, b) => b - a);
-
-    for (let i = 0; i < keys.length; i++) {
-        let num = Number(keys[i]);
-        if (obj[num] === num) {
-            return num;
+    for (let key in obj) {
+        if (obj[key] == key) {
+            lucky = key;
         }
     }
 
-    return -1;
+    return lucky;
 
 };
+
+// TC => O(n);
+// SC => O(n);
+
+// console.log(findLucky(arr = [1,2,2,3,3,3]));
+
+
+// Solution-2:
+// var findLucky = function(arr) {
+    
+//     let obj = {};
+
+//     for (let i = 0; i < arr.length; i++) {
+//         if (obj[arr[i]]) {
+//             obj[arr[i]]++;
+//         }
+//         else {
+//             obj[arr[i]] = 1;
+//         }
+//     }
+
+//     let keys = Object.keys(obj);
+//     keys.sort((a, b) => b - a);
+
+//     for (let i = 0; i < keys.length; i++) {
+//         let num = Number(keys[i]);
+//         if (obj[num] === num) {
+//             return num;
+//         }
+//     }
+
+//     return -1;
+
+// };
 
 // TC => O(n*log(n));
 // SC => O(n);
 
-console.log(findLucky(arr = [2,2,2,3,3]));
+// console.log(findLucky(arr = [2,2,2,3,3]));
 

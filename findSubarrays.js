4b3aaa8deaaae8ee71884ea2452503911c857151
Rohan Palankar <rohanpalankar8@gmail.com>
2023-07-06 14:16:40 +0530

    file findSubarrays.js updated

--- findSubarrays.js
+++ findSubarrays.js
@@ -29,3 +29,21 @@
 
 // Leetcode link: https://leetcode.com/problems/find-subarrays-with-equal-sum/description/
 
+// Solution:
+
+var findSubarrays = function(nums) {
+    
+    for (let i = 0; i < nums.length-2; i++) {
+        let sum1 = nums[i] + nums[i+1];
+        for (let j = i+1; j < nums.length-1;j++) {
+            let sum2 = nums[j] + nums[j+1];
+            if (sum1 === sum2) {
+                return true;
+            }
+        }
+    } 
+
+    return false;
+
+};
+

b3347ca6da11b5a9d03e8e48f9beb1d73162553f
Rohan Palankar <rohanpalankar8@gmail.com>
2023-07-06 14:42:44 +0530

    file majorityElement.js updated

--- majorityElement.js
+++ majorityElement.js
@@ -15,3 +15,35 @@
 // Input: nums = [2,2,1,1,1,2,2]
 // Output: 2
 
+// Leetcode link: https://leetcode.com/problems/majority-element/description/
+
+// Solution:
+
+var majorityElement = function(nums) {
+    
+    let n = nums.length;
+    let obj = {};
+
+    for (let i = 0; i < n; i++) {
+        if (obj[nums[i]]) {
+            obj[nums[i]]++;
+        }
+        else {
+            obj[nums[i]] = 1;
+        }
+        if (obj[nums[i]] > n/2) {
+            return nums[i];
+        }
+    }
+
+    // for (let num in obj) {
+    //     if (obj[num] > n/2) {
+    //         return num;
+    //     }
+    // }
+
+};
+
+// TC => O(n)
+// SC => O(n)
+

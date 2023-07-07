9b11f0f28cec29c790349d64a079d9337417141b
Rohan Palankar <rohanpalankar8@gmail.com>
2023-07-07 14:39:34 +0530

    file containsDuplicate.js updated

--- containsDuplicate.js
+++ containsDuplicate.js
@@ -18,4 +18,42 @@
 // Input: nums = [1,1,1,3,3,4,3,2,4,2]
 // Output: true
 
+// Leetcode link: https://leetcode.com/problems/contains-duplicate/description/
+
+// Solution:
+
+var containsDuplicate = function(nums) {
+    
+    // if (nums.length < 2) return false;
+
+    // for (let i = 0; i < nums.length; i++) {
+    //     let int = nums[i];
+    //     if (nums.indexOf(int) != nums.lastIndexOf(int)) {
+    //         return true;
+    //     }
+    // }
+
+    // return false; Time limit exceeds.
+
+    if (nums.length < 2) return false;
+
+    let obj = {};
+
+    for (let i = 0; i < nums.length; i++) {
+        let int = nums[i];
+        if (obj[int]) {
+            return true;
+        }
+        else {
+            obj[int] = 1;
+        }
+    }
+
+    return false;
+
+};
+
+// TC => O(n);
+// SC => O(n);
+
 

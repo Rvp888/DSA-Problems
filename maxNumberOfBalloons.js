483ab1e3ee95004801faed58681ee63d3c308505
Rohan Palankar <rohanpalankar8@gmail.com>
2023-07-08 12:13:11 +0530

    file maxNumberOfBalloons.js updated

--- maxNumberOfBalloons.js
+++ maxNumberOfBalloons.js
@@ -19,3 +19,24 @@
 // Input: text = "leetcode"
 // Output: 0
 
+// Leetcode link: https://leetcode.com/problems/maximum-number-of-balloons/description/
+
+// Solution:
+
+var maxNumberOfBalloons = function(text) {
+    
+    let obj = {"b":0, "a":0, "l":0, "o":0, "n":0};
+
+    for (let i = 0; i < text.length; i++) {
+        if (text[i] == "b" || text[i] == "a" || text[i] == "l" || text[i] == "o" || text[i] == "n") {
+                obj[text[i]]++;
+        }
+    }
+
+    return Math.min(obj['b'], obj['a'], obj['n'], Math.floor(obj['o']/2), Math.floor(obj['l']/2));
+
+};
+
+// TC => O(n)
+// SC => O(n)
+

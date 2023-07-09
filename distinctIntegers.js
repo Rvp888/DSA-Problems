224861ceb19dd5a3fdfd91a0ae9a398d4cc9ad47
Rohan Palankar <rohanpalankar8@gmail.com>
2023-07-09 13:00:38 +0530

    file distinctIntegers.js updated

--- distinctIntegers.js
+++ distinctIntegers.js
@@ -13,3 +13,20 @@
 // * Once a number is placed on the board, it will remain on it until the end.
 // * % stands for the modulo operation. For example, 14 % 3 is 2.
 
+// Example 1:
+
+// Input: n = 5
+// Output: 4
+// Explanation: Initially, 5 is present on the board. 
+// The next day, 2 and 4 will be added since 5 % 2 == 1 and 5 % 4 == 1. 
+// After that day, 3 will be added to the board because 4 % 3 == 1. 
+// At the end of a billion days, the distinct numbers on the board will be 2, 3, 4, and 5. 
+
+// Example 2:
+
+// Input: n = 3
+// Output: 2
+// Explanation: 
+// Since 3 % 2 == 1, 2 will be added to the board. 
+// After a billion days, the only two distinct numbers on the board are 2 and 3. 
+

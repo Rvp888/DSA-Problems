7dee958eea7156352206ba22355a4c6e9ce374af
Rohan Palankar <rohanpalankar8@gmail.com>
2023-07-11 12:02:46 +0530

    file makeEqual.js updated

--- makeEqual.js
+++ makeEqual.js
@@ -7,4 +7,18 @@
 
 // Return true if you can make every string in words equal using any number of operations, and false otherwise.
 
+// Example 1:
+
+// Input: words = ["abc","aabc","bc"]
+// Output: true
+// Explanation: Move the first 'a' in words[1] to the front of words[2],
+// to make words[1] = "abc" and words[2] = "abc".
+// All the strings are now equal to "abc", so return true.
+
+// Example 2:
+
+// Input: words = ["ab","a"]
+// Output: false
+// Explanation: It is impossible to make all the strings equal using the operation.
+
 

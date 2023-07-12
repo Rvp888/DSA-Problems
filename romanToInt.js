66f62d7825e1f4d928ea4e02e099d69fbcf23e88
Rohan Palankar <rohanpalankar8@gmail.com>
2023-07-12 11:51:32 +0530

    file romanToInt.js updated

--- romanToInt.js
+++ romanToInt.js
@@ -25,4 +25,57 @@
 // Output: 1994
 // Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
 
+// Leetcode link: https://leetcode.com/problems/roman-to-integer/description/
+
+// Solution:
+
+var romanToInt = function(s) {
+    
+    let result = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === 'M') {
+            if (s[i-1] === 'C') {
+                result -= 200;
+            }
+            result += 1000;
+        }
+        else if (s[i] === 'D') {
+            if (s[i-1] === 'C') {
+                result -= 200;
+            }
+            result += 500;
+        }
+        else if (s[i] === 'C') {
+            if (s[i-1] === 'X') {
+                result -= 20;
+            }
+            result += 100;
+        }
+        else if (s[i] === 'L') {
+            if (s[i-1] === 'X') {
+                result -= 20;
+            }
+            result += 50;
+        }
+        else if (s[i] === 'X') {
+            if (s[i-1] === 'I') {
+                result -= 2;
+            }
+            result += 10;
+        }
+        else if (s[i] === 'V') {
+            if (s[i-1] === 'I') {
+                result -= 2;
+            }
+            result += 5;
+        }
+        else if (s[i] === "I") {
+            result += 1;
+        }
+    }
+
+    return result;
+
+};
 

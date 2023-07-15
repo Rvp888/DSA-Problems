9de66e9e5a084af9172df6ea7ad3af97b74f5c86
Rohan Palankar <rohanpalankar8@gmail.com>
2023-07-15 10:57:39 +0530

    file rearrangeCharacters.js updated

--- rearrangeCharacters.js
+++ rearrangeCharacters.js
@@ -4,3 +4,21 @@
 
 // Return the maximum number of copies of target that can be formed by taking letters from s and rearranging them.
 
+// Example 1:
+
+// Input: s = "ilovecodingonleetcode", target = "code"
+// Output: 2
+// Explanation:
+// For the first copy of "code", take the letters at indices 4, 5, 6, and 7.
+// For the second copy of "code", take the letters at indices 17, 18, 19, and 20.
+// The strings that are formed are "ecod" and "code" which can both be rearranged into "code".
+// We can make at most two copies of "code", so we return 2.
+
+// Example 3:
+
+// Input: s = "abbaccaddaeea", target = "aaaaa"
+// Output: 1
+// Explanation:
+// We can make one copy of "aaaaa" by taking the letters at indices 0, 3, 6, 9, and 12.
+// We can make at most one copy of "aaaaa", so we return 1.
+

8937770315bf0e62a70dc296b1484e094a5b52c3
Rohan Palankar <rohanpalankar8@gmail.com>
2023-07-17 21:39:09 +0530

    file intersect.js updated

--- intersect.js
+++ intersect.js
@@ -15,3 +15,26 @@
 // Output: [4,9]
 // Explanation: [9,4] is also accepted.
 
+// Leetcode link: https://leetcode.com/problems/intersection-of-two-arrays-ii/description/
+
+// Solution:
+
+var intersect = function(nums1, nums2) {
+    
+    let result = [];
+
+    for (let i = 0; i < nums1.length; i++) {
+        if (nums2.length == 0) {
+            break;
+        }
+        if (nums2.includes(nums1[i])) {
+            result.push(nums1[i]);
+            let index = nums2.indexOf(nums1[i]);
+            nums2.splice(index, 1);
+        }
+    }
+
+    return result;
+
+};
+

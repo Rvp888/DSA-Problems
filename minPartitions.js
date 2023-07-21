4e47f3884b535a652c332936f3274edf0c42aeaa
Rohan Palankar <rohanpalankar8@gmail.com>
2023-07-21 20:16:20 +0530

    file minPartitions.js updated

--- minPartitions.js
+++ minPartitions.js
@@ -6,3 +6,19 @@
 // Given a string n that represents a positive decimal integer, return the minimum number of 
 // positive deci-binary numbers needed so that they sum up to n.
 
+// Example 1:
+
+// Input: n = "32"
+// Output: 3
+// Explanation: 10 + 11 + 11 = 32
+
+// Example 2:
+
+// Input: n = "82734"
+// Output: 8
+
+// Example 3:
+
+// Input: n = "27346209830709182346"
+// Output: 9
+

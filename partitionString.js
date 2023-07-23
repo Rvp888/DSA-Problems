5dffbfddaa39344170ebb3bbe75cfbf88411ff9e
Rohan Palankar <rohanpalankar8@gmail.com>
2023-07-23 11:48:37 +0530

    file partitionString.js updated

--- partitionString.js
+++ partitionString.js
@@ -7,4 +7,19 @@
 
 // Note that each character should belong to exactly one substring in a partition.
 
+// Example 1:
+
+// Input: s = "abacaba"
+// Output: 4
+// Explanation:
+// Two possible partitions are ("a","ba","cab","a") and ("ab","a","ca","ba").
+// It can be shown that 4 is the minimum number of substrings needed.
+
+// Example 2:
+
+// Input: s = "ssssss"
+// Output: 6
+// Explanation:
+// The only valid partition is ("s","s","s","s","s","s").
+
 

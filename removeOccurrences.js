f424427bc366022f9cca68f1a2b961d73af2d6fa
Rohan Palankar <rohanpalankar8@gmail.com>
2023-07-24 18:41:21 +0530

    file removeOccurrences.js updated

--- removeOccurrences.js
+++ removeOccurrences.js
@@ -8,4 +8,14 @@
 
 // A substring is a contiguous sequence of characters in a string.
 
+// Example 1:
+
+// Input: s = "daabcbaabcbc", part = "abc"
+// Output: "dab"
+// Explanation: The following operations are done:
+// - s = "daabcbaabcbc", remove "abc" starting at index 2, so s = "dabaabcbc".
+// - s = "dabaabcbc", remove "abc" starting at index 4, so s = "dababc".
+// - s = "dababc", remove "abc" starting at index 3, so s = "dab".
+// Now s has no occurrences of "abc".
+
 

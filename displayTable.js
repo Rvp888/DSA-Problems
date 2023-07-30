b24fc5608926eea3d6b95739494e8be37374c95d
Rohan Palankar <rohanpalankar8@gmail.com>
2023-07-30 21:35:24 +0530

    file displayTable.js updated

--- displayTable.js
+++ displayTable.js
@@ -11,3 +11,17 @@
 // first column is “Table”, followed by the names of the food items. Note that the customer names are 
 // not part of the table. Additionally, the rows should be sorted in numerically increasing order.
 
+// Example 1:
+
+// Input: orders = [["David","3","Ceviche"],["Corina","10","Beef Burrito"],["David","3","Fried Chicken"],["Carla","5","Water"],["Carla","5","Ceviche"],["Rous","3","Ceviche"]]
+// Output: [["Table","Beef Burrito","Ceviche","Fried Chicken","Water"],["3","0","2","1","0"],["5","0","1","0","1"],["10","1","0","0","0"]] 
+// Explanation:
+// The displaying table looks like:
+// Table,Beef Burrito,Ceviche,Fried Chicken,Water
+// 3    ,0           ,2      ,1            ,0
+// 5    ,0           ,1      ,0            ,1
+// 10   ,1           ,0      ,0            ,0
+// For the table 3: David orders "Ceviche" and "Fried Chicken", and Rous orders "Ceviche".
+// For the table 5: Carla orders "Water" and "Ceviche".
+// For the table 10: Corina orders "Beef Burrito".
+

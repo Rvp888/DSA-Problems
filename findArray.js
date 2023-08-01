867274e85e5ba098630e95187c9450f47791dc0e
Rohan Palankar <rohanpalankar8@gmail.com>
2023-08-01 20:59:41 +0530

    file findArray.js updated

--- findArray.js
+++ findArray.js
@@ -8,3 +8,14 @@
 
 // It can be proven that the answer is unique.
 
+// Example 1:
+
+// Input: pref = [5,2,0,3,1]
+// Output: [5,7,2,3,2]
+// Explanation: From the array [5,7,2,3,2] we have the following:
+// - pref[0] = 5.
+// - pref[1] = 5 ^ 7 = 2.
+// - pref[2] = 5 ^ 7 ^ 2 = 0.
+// - pref[3] = 5 ^ 7 ^ 2 ^ 3 = 3.
+// - pref[4] = 5 ^ 7 ^ 2 ^ 3 ^ 2 = 1.
+

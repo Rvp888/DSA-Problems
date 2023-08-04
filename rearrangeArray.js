46f946af8f96326bf78f13088001e6dce8113d20
Rohan Palankar <rohanpalankar8@gmail.com>
2023-08-04 17:04:53 +0530

    file rearrangeArray.js updated

--- rearrangeArray.js
+++ rearrangeArray.js
@@ -19,3 +19,36 @@
 // The only possible way to rearrange them such that they satisfy all conditions is [3,-2,1,-5,2,-4].
 // Other ways such as [1,-2,2,-5,3,-4], [3,1,2,-2,-5,-4], [-2,3,-5,1,-4,2] are incorrect because they do not satisfy one or more conditions.  
 
+// Leetcode link: https://leetcode.com/problems/rearrange-array-elements-by-sign/description/
+
+// Solution:
+
+var rearrangeArray = function(nums) {
+
+    let output = [];
+    let evens = [], odds = [];
+    
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] > 0) {
+            evens.push(nums[i]);
+        }
+        else {
+            odds.push(nums[i]);
+        }
+    }
+
+    let i = 0;
+
+    while (i < evens.length) {
+        output.push(evens[i]);
+        output.push(odds[i]);
+        i++;
+    }
+
+    return output;
+
+};
+
+// TC => O(n^2)
+// SC => O(n)
+

cd56d3fca7a5e6baa919aff1fea3b2e230c66ce3
Rohan Palankar <rohanpalankar8@gmail.com>
2023-08-10 17:19:51 +0530

    file findAndReplacePattern.js updated

--- findAndReplacePattern.js
+++ findAndReplacePattern.js
@@ -9,3 +9,17 @@
 // Recall that a permutation of letters is a bijection from letters to letters: every letter maps to 
 // another letter, and no two letters map to the same letter.
 
+// Example 1:
+
+// Input: words = ["abc","deq","mee","aqq","dkd","ccc"], pattern = "abb"
+// Output: ["mee","aqq"]
+// Explanation: "mee" matches the pattern because there is a permutation
+//  {a -> m, b -> e, ...}. 
+// "ccc" does not match the pattern because {a -> c, b -> c, ...} is not
+//  a permutation, since a and b map to the same letter.
+
+// Example 2:
+
+// Input: words = ["a","b","c"], pattern = "a"
+// Output: ["a","b","c"]
+

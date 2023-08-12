580b8811fde87ef1b5abca5261f337b0fb094a40
Rohan Palankar <rohanpalankar8@gmail.com>
2023-08-12 16:34:37 +0530

    file numOfPairs.js updated

--- numOfPairs.js
+++ numOfPairs.js
@@ -3,3 +3,14 @@
 // Given an array of digit strings nums and a digit string target, return the number of pairs 
 // of indices (i, j) (where i != j) such that the concatenation of nums[i] + nums[j] equals target.
 
+// Example 1:
+
+// Input: nums = ["777","7","77","77"], target = "7777"
+// Output: 4
+// Explanation: Valid pairs are:
+// - (0, 1): "777" + "7"
+// - (1, 0): "7" + "777"
+// - (2, 3): "77" + "77"
+// - (3, 2): "77" + "77"
+
+

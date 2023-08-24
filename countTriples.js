70c956c592473cb46223badf71fdb7ab4c8aad14
Rohan Palankar <rohanpalankar8@gmail.com>
2023-08-25 00:21:48 +0530

    file countTriples.js updated

--- countTriples.js
+++ countTriples.js
@@ -15,3 +15,24 @@
 // Input: n = 10
 // Output: 4
 // Explanation: The square triples are (3,4,5), (4,3,5), (6,8,10), and (8,6,10).
+
+// Leetcode link: https://leetcode.com/problems/count-square-sum-triples/description/
+
+// Solution-1:
+
+var countTriples = function (n) {
+  let result = 0;
+
+  for (let a = 3; a < n; a++) {
+    for (let b = a + 1; b < n; b++) {
+      let c = Math.sqrt(a * a + b * b);
+      if (Number.isInteger(c) && c <= n) {
+        result += 2;
+      }
+    }
+  }
+
+  return result;
+};
+
+console.log(countTriples((n = 10)));
